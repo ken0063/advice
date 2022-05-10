d288ca901413406aedf376407637e9fb7cc14d5f
ken0063 <olukotunk@gmail.com>
2022-05-10 11:34:47 +0100

    Advice generator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import "./App.css";
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+import "./App.scss";
 
 function App() {
-  return <div>Hello</div>;
+  const [data, setData] = useState("Loading");
+
+  const advice = useCallback(async () => {
+    try {
+      const response = await axios.get("https://api.adviceslip.com/advice");
+      setData(response?.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [setData]);
+
+  useEffect(() => {
+    advice();
+  }, [advice]);
+
+  return (
+    <div className="app">
+      <div className="container">
+        <p>{!data?.slip?.id ? "Loading..." : `ADVICE  #${data?.slip?.id}`}</p>
+        <span>
+          {!data?.slip?.advice ? "Loading..." : `"${data?.slip?.advice}"`}
+        </span>
+        <img
+          src="./images/pattern-divider-mobile.svg"
+          alt="pattern-divider-mobile"
+        />
+
+        <button type="button" onClick={advice}>
+          <img src="./images/icon-dice.svg" alt="icon-dice" />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
